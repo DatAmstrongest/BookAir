c5b4792ef359c7a7a920b8170db0916678794851
DatAmstrongest <metehan.koc@aalto.fi>
2026-02-19 12:25:04 +0000

    Database config file is added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
 import userRoutes from './routes/userRoutes';
 import errorHandler from './middlewares/errorHandler';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Application = express();
-const port: number = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use('/api', userRoutes);
 app.use(errorHandler);

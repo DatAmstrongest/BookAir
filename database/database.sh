4627fe9343df7d8494e498665b6795208d3d157c
DatAmstrongest <metehan.koc@aalto.fi>
2026-02-19 11:51:43 +0000

    Database script is updated

--- database/database.sh
+++ database/database.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-
-podman run --detach --name bookingDB -p 5000:27017 docker.io/mongodb/mongodb-community-server:latest
+if podman ps -a | grep "bookingDB"; then
+	if podman ps | grep "bookingDB"; then
+		echo "Database is running already"
+	else
+		podman start $(podman ps -a -q --filter "name=bookingDB")
+		echo "Database is started"
+	fi
+else
+	podman run --detach --name bookingDB -p 5000:27017 docker.io/mongodb/mongodb-community-server:latest
+	echo "Database container is created and database started"
+fi

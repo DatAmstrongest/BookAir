419cd74bf65d0599af52544442f2c098dfded1cb
DatAmstrongest <metehan.koc@aalto.fi>
2026-02-19 12:15:17 +0000

    User interface is created and model is updated

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,3 +1,20 @@
-export default class User {
-	constructor (public name: string, public email: string, public id?: ObjectId) {}
-}
+import { Schema, model } from 'mongoose';
+import { IUser } from '../interfaces/IUser';
+
+const UserSchema = new Schema<IUser>({
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
+	},
+	id: {
+		type: Schema.Types.ObjectId,
+	}
+});

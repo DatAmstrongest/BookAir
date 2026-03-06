8a1c3890c556128ed5f542bf7e6871ce7a46a93a
DatAmstrongest <metehan.koc@aalto.fi>
2026-03-06 14:03:52 +0000

    Home, Login and register routes are added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from "./pages/Home.tsx";
+import Register from './pages/Register.tsx';
+import Login from './pages/Login.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/Register">Register</Link> |{" "}
+        <Link to="/Login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
